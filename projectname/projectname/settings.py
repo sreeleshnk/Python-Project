8177f3eafdbf2fc4c32040854e2ee53b5f3fdaea
shamal94 <shamalfernando123@gmail.com>
2022-12-16 00:05:25 +0000

    settings and url

--- projectname/projectname/settings.py
+++ projectname/projectname/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(_file_).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'book',
-    'order',
-    'blog',
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +55,7 @@ ROOT_URLCONF = 'projectname.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["books/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
